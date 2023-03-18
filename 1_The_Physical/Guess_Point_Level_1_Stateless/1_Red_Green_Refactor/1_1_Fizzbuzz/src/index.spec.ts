7683a2fd5aa19f0eb5811ce50ec19a3a8f7666a7
Jonathan Howard <jonathanhoward@fastmail.com>
2023-03-18 16:08:59 -0400

    Can convert a number to a string

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,10 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it.each([1, 4, 8])(
+    "should change non-multiples of 3 and/or 5 to a string",
+    (x) => {
+      expect(fizzbuzz(x)).toBe(x.toString());
+    }
+  );
 });
